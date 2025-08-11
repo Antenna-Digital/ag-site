898ac90af5a6ecf37e0f3a476092acf0bc509934
Tyler Hall <tylerbhall11@gmail.com>
2025-08-11 16:43:18 -0400

    Add workSectionScrollLock

--- scripts.js
+++ scripts.js
@@ -115,12 +115,75 @@ function swipers() {
   }
 };
 
+function workSectionScrollLock(){
+  // Initialize all carousel sections on the page
+  document.querySelectorAll('.work-sl_contain').forEach((container, containerIndex) => {
+    
+    // Get elements within this specific container
+    const carouselLayout = container.querySelector('.work-sl_layout.is-carousel-layout');
+    const collectionWrap = container.querySelector('.work-sl_collection_wrap');
+    const collectionList = container.querySelector('.work-sl_collection_list');
+    const collectionItems = container.querySelectorAll('.work-sl_collection_item');
+    const progressBar = container.querySelector('.work-sl_carousel_progress');
+
+    // Function to calculate scroll distance (will be called on refresh)
+    const getScrollDistance = () => {
+      const computedStyle = window.getComputedStyle(collectionList);
+      const paddingLeft = parseFloat(computedStyle.paddingLeft);
+      const paddingRight = parseFloat(computedStyle.paddingRight);
+      const totalPadding = paddingLeft + paddingRight;
+      return collectionWrap.scrollWidth - window.innerWidth + totalPadding;
+    };
+    
+    // Create timeline for this specific container
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: carouselLayout,
+        start: 'center center',
+        end: () => `+=${getScrollDistance() * 1.5}`, // Function-based value
+        scrub: 1,
+        pin: true,
+        invalidateOnRefresh: true,
+        pinSpacing: true,  // Explicitly set pin spacing
+        onUpdate: (self) => {
+          // Update progress bar width based on scroll progress
+          if (progressBar) {
+            gsap.set(progressBar, {
+              width: `${self.progress * 100}%`
+            });
+          }
+        }
+      }
+    });
+    
+    // Horizontal scroll animation
+    tl.to(collectionList, {
+      x: () => -getScrollDistance(), // Function-based value
+      ease: 'none'
+    });
+    
+    collectionItems.forEach(item => {
+    });
+    
+  });
+
+  // Refresh ScrollTrigger on window resize
+  let resizeTimer;
+  window.addEventListener('resize', () => {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      ScrollTrigger.refresh(true); // Force refresh
+    }, 250); // Debounce resize
+  });
+}
+
 // Init Function
 const init = () => {
   console.debug("%cRun init", "color: lightgreen;");
 
   setupLenis();
   swipers();
+  workSectionScrollLock();
 }; // end init
 
 $(window).on("load", init);
