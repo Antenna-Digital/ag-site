3e9404d1de07eff44ef93ac9f45699df3b0aa258
Tyler Hall <tylerbhall11@gmail.com>
2025-11-03 13:32:25 -0500

    Testing odometer fixes

--- scripts.js
+++ scripts.js
@@ -844,6 +844,15 @@ function odometers() {
 
                   gsap.delayedCall(delay, function () {
                     od.update(originalValue);
+      
+                    // iOS fix: Force layout recalculation after animation
+                    if (isIOS) {
+                      gsap.delayedCall(3.2, function() { // Slightly after 3s duration
+                        statVal.style.display = 'inline-block';
+                        void statVal.offsetWidth; // Force reflow
+                        statVal.classList.add('force-block');
+                      });
+                    }
                   });
                 },
               },
